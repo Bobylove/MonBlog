fd6190ac4911c75671b4b2a9092f51c043a4cc4a
Fred <delon.frederic.pro.job@gmail.com>
2017-04-12 23:08:18 +0200

    db seed add

--- app/User.php
+++ app/User.php
@@ -18,14 +18,18 @@ class User extends Authenticatable
      *
      * @var array
      */
-   
+    protected $fillable = [
+    'username', 'email', 'password',
+    ];
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    
+    protected $hidden = [
+    'password', 'remember_token',
+    ];
     public static function boot(){
         parent::boot();
         
