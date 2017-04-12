641f0d65deef793a9a705a5c45f532d521a5beaa
Fred <delon.frederic.pro.job@gmail.com>
2017-04-12 21:27:09 +0200

    try again

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,13 +17,23 @@
 <div class="container" >
 	<div class="row">
 		<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-			
+			@foreach($datePost as $date)
+			@if($date->publier == 1 ) 
+			<div class="post-preview">
+				<a href="{{ URL::route('posts.show',$date->slug)}}">
+					<h2 class="post-title animated  slideInRight">{{ $date->name }}</h2>
+				</h2>
+			</a>
+			<p class="post-meta animated  slideInLeft blog-post-meta">{{ $date->created_at->diffForHumans() }} by <a href="{{ URL::route('posts.show',$date->slug)}}">{{ $user->firstname}}</a></p>
+			<hr>
+			@endif
+			@endforeach
 		</div>
 	</div>
 
 	<ul class="pager">
 		<li class="next">
-	
+			{{$datePost->Links()}}
 		</li>
 	</ul>
 
