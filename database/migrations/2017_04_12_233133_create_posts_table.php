b83c8eaaf2730aba039fe3f8f2935990dd284343
Fred <delon.frederic.pro.job@gmail.com>
2017-04-13 02:14:27 +0200

    hum

--- database/migrations/2017_04_12_233133_create_posts_table.php
+++ database/migrations/2017_04_12_233133_create_posts_table.php
@@ -14,7 +14,7 @@ class CreatePostsTable extends Migration
     public function up()
     {
                Schema::create('posts', function (Blueprint $table) {
-        $table->increments('id')->nullable();
+        $table->increments('id');
         $table->string('name')->nullable();
         $table->string('slug')->nullable();
         $table->text('content')->nullable();
