6f0742a37cdef081fc69156f23ef8ffc17bfb5f9
Fred <delon.frederic.pro.job@gmail.com>
2017-05-01 03:36:47 +0200

    try

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -16,7 +16,7 @@
 	@endif
 </p>
 
-<p id="summernote">{{ $post->content }}</p>
+<p>{{ htmlspecialchars($post->content, ENT_QUOTES, "UTF-8") }}</p>
 
 @if($post->counts_comment > 0)
 <h3>Les Commentaires</h3>
