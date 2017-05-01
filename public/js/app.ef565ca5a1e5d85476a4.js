0895a8209f89767676878458d86cae22635b0156
Fred <delon.frederic.pro.job@gmail.com>
2017-05-01 02:42:20 +0200

    wysiwyg

--- public/js/app.ef565ca5a1e5d85476a4.js
+++ public/js/app.ef565ca5a1e5d85476a4.js
@@ -75,6 +75,7 @@ $(document).ready(function () {
 	$(".alert").fadeTo(2000, 500).slideUp(1000, function () {
 		$('.alert').remove();
 	});
+	$('.content').summernote();
 });
 
 /***/ }),
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 	$(".alert").fadeTo(2000, 500).slideUp(1000, function(){
 		$('.alert').remove();
 	});
-
+$('.content').summernote();
 	
 });
+
