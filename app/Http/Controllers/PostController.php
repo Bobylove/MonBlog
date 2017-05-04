eb77bd5f75c1108aaaf130231bf61567bab0da6f
Fred <delon.frederic.pro.job@gmail.com>
2017-05-04 15:50:23 +0200

    try

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -27,7 +27,8 @@ public function welcome(){
 	public function index(){
 		Carbon::setLocale('fr');
 		$datePost = Post::latest()->where('publier','=',1)->paginate(2);
-		$name = 1; // besoin de débuge la relation user /post */
+		$user = $post->user;
+		$name = $user; // besoin de débuge la relation user /post */
 		$post = Post::where('user_id',$name)->FirstOrFail();
 		$user = $post->user;
 		
